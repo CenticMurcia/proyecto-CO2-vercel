4d5e5e334e53540d7b14cb5d231d2673ac9d06c0
Javi <franfdk17@gmail.com>
2023-02-08 14:57:41 +0100

    minimal version

--- app.py
+++ app.py
@@ -3,46 +3,55 @@
 # $ python app.py 
 
 import requests
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+#from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from datetime import datetime
 import pytz
 import os
-import numpy as np
-import pandas as pd
 
 
 app = Flask(__name__)
 
-hora_list = ["Iniciando..."]
-CO2_list  = [-1]
-PM10_list = [-1]
-PM25_list = [-1]
-CO2_msg  = ""
-PM10_msg = ""
-PM25_msg = ""
+hist_Hora = []
+hist_CO2  = []
+hist_PM10 = []
+hist_PM25 = []
+hist_Temperatura = []
+hist_Humedad = []
+hist_PersonasIn = []
+hist_PersonasOut = []
+hist_Personas = []
 
+show_Hora = ["Iniciando..."]
+show_CO2  = [-1]
+show_PM10 = [-1]
+show_PM25 = [-1]
+CO2_msg   = ""
+PM10_msg  = ""
+PM25_msg  = ""
 
 #################################### READ HOPU DATA
 
 def get_datetime():
     # datetime object containing current date and time
     # dd/mm/YY H:M:S
-    global date,time,hora
     dt = datetime.now(pytz.timezone("Europe/Madrid"))
     date = dt.strftime("%d/%m/%Y")
     time = dt.strftime("%H:%M:%S")
     hora = dt.strftime("%H:%M")
+
+    hist_Hora.append( hora )
+
 
 
 # 1. Iniciar sesion en el APIRest de Hopu
 #    Obtener access token y refress token
 
 def API_get_token():
-    url     = "https://fiware.hopu.eu/keycloak/auth/realms/fiware-server/protocol/openid-connect/token" 
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    data    = "username=julgonzalez&password=vZnAWE7FexwgEqwT&grant_type=password&client_id=fiware-login"
+    url      = "https://fiware.hopu.eu/keycloak/auth/realms/fiware-server/protocol/openid-connect/token" 
+    headers  = {"Content-Type": "application/x-www-form-urlencoded"}
+    data     = "username=julgonzalez&password=vZnAWE7FexwgEqwT&grant_type=password&client_id=fiware-login"
     response = requests.post(url, data = data, headers = headers).json()
 
     global access_token, refresh_token
@@ -53,8 +62,8 @@ def API_get_token():
 
 def API_get_device_status(access_token):
 
-    url     = "https://fiware.hopu.eu/orion/v2/entities?limit=1000&attrs=*,dateModified&options=count,keyValues" 
-    headers = {"fiware-service": "Device", "fiware-servicepath": "/ctcon", "Authorization": "Bearer "+access_token}
+    url      = "https://fiware.hopu.eu/orion/v2/entities?limit=1000&attrs=*,dateModified&options=count,keyValues" 
+    headers  = {"fiware-service": "Device", "fiware-servicepath": "/ctcon", "Authorization": "Bearer "+access_token}
     response = requests.get(url, headers = headers).json()[0]
 
     global operationalStatus
@@ -62,65 +71,30 @@ def API_get_device_status(access_token):
     operationalStatus = response["operationalStatus"]
 
 
+
 def API_get_calidad_aire(access_token):
 
-    url     = "https://fiware.hopu.eu/orion/v2/entities?limit=1000&attrs=*,dateModified&options=keyValues" 
-    headers = {"fiware-service": "AirQuality", "fiware-servicepath": "/ctcon", "Authorization": "Bearer "+access_token}
+    url      = "https://fiware.hopu.eu/orion/v2/entities?limit=1000&attrs=*,dateModified&options=keyValues" 
+    headers  = {"fiware-service": "AirQuality", "fiware-servicepath": "/ctcon", "Authorization": "Bearer "+access_token}
     response = requests.get(url, headers = headers).json()[0]
 
-    global CO2,PM10,PM25,Temperatura,Humedad    
-
-    CO2         = response["CO2"]
-    PM10        = response["PM10"]
-    PM25        = response["PM25"]
-    Temperatura = response["temperature"]
-    Humedad     = response["humidity"]
+    hist_CO2.append(         response["CO2"]         )
+    hist_PM10.append(        response["PM10"]        )
+    hist_PM25.append(        response["PM25"]        )
+    hist_Temperatura.append( response["temperature"] )
+    hist_Humedad.append(     response["humidity"]    )
 
 
 
 def API_get_presencia(access_token):
 
-    url     = "https://fiware.hopu.eu/orion/v2/entities?limit=1000&attrs=*,dateModified&options=count,keyValues" 
-    headers = {"fiware-service": "PeopleCounting", "fiware-servicepath": "/ctcon", "Authorization": "Bearer "+access_token}
+    url      = "https://fiware.hopu.eu/orion/v2/entities?limit=1000&attrs=*,dateModified&options=count,keyValues" 
+    headers  = {"fiware-service": "PeopleCounting", "fiware-servicepath": "/ctcon", "Authorization": "Bearer "+access_token}
     response = requests.get(url, headers = headers).json()[0]
 
-    global PersonasIn,PersonasOut,Personas
-
-    PersonasIn  = response["numberOfIncoming"]
-    PersonasOut = response["numberOfOutgoing"]
-    Personas    = PersonasIn - PersonasOut
-
-
-
-####################################
-
-def init_empty_data():
-    f = open("tmp/data.csv", "w")
-    f.write("Fecha,Hora,PersonasIn,PersonasOut,Personas,Temperatura,Humedad,CO2,PM10,PM25\n")
-    f.close()
-
-def save_data():
-    f = open('tmp/data.csv', 'a')
-    f.write(date+","+hora+","+
-            str(PersonasIn)+","+
-            str(PersonasOut)+","+
-            str(Personas)+","+
-            str(Temperatura)+","+
-            str(Humedad)+","+
-            str(CO2)+","+
-            str(PM10)+","+
-            str(PM25)+"\n")
-    f.close()
-
-def print_data():
-    print("   PersonasIn  = ", PersonasIn)
-    print("   PersonasOut = ", PersonasOut)
-    print("   Personas    = ", Personas)
-    print("   Temperatura = ", Temperatura)
-    print("   Humedad     = ", Humedad)
-    print("   CO2         = ", CO2)
-    print("   PM10        = ", PM10)
-    print("   PM25        = ", PM25)
+    hist_PersonasIn.append(  response["numberOfIncoming"] )
+    hist_PersonasOut.append( response["numberOfOutgoing"] )
+    hist_Personas.append(    PersonasIn - PersonasOut     )
 
 
 
@@ -186,33 +160,38 @@ def get_predictions(observed_array, n_points_to_predict):
 
 
 
+hist_Hora = []
+hist_CO2  = []
+hist_PM10 = []
+hist_PM25 = []
+hist_Temperatura = []
+hist_Humedad = []
+hist_PersonasIn = []
+hist_PersonasOut = []
+hist_Personas = []
+
+show_Hora = ["Iniciando..."]
+show_CO2  = [-1]
+show_PM10 = [-1]
+show_PM25 = [-1]
+CO2_msg   = ""
+PM10_msg  = ""
+PM25_msg  = ""
+
+
 def get_ml_predictions():
 
-    global hora_list, CO2_list, PM10_list, PM25_list, CO2_msg, PM10_msg, PM25_msg
-    global ml_model
-
-    # get tail(4) that means lag15, lag10, lag5, actual 
-    in_dat = pd.read_csv("tmp/data.csv").tail(4)
-
-    hora_hist = in_dat["Hora"].values
-    temp_hist = in_dat["Temperatura"].values # np array [temp_lag15, temp_lag10, temp_lag5, temp_actual] 
-    hume_hist = in_dat["Humedad"].values
-    pm25_hist = in_dat["PM25"].values
-    pm10_hist = in_dat["PM10"].values
-    CO2_hist  = in_dat["CO2"].values
-    pers_hist = in_dat["Personas"].values
-
-    if len(in_dat) == 4:
+    if len(hist_Hora) >= 4:
 
         #### ENOUGH DATA -> DO ML PREDICTION
 
-        hora_list = list(hora_hist) + ["+5 mins", "+10 mins", "+15 mins", "+20 mins"]
-        PM25_list = list(pm25_hist) + get_predictions(pm25_hist, 4)
-        PM10_list = list(pm10_hist) + get_predictions(pm10_hist, 4)
-        CO2_list  = list(CO2_hist)  + get_predictions(CO2_hist, 4)
-        CO2_msg   = get_CO2_msg(CO2_list[-1])
-        PM10_msg  = get_PM10_msg(PM10_list[-1])
-        PM25_msg  = get_PM25_msg(PM25_list[-1])
+        show_Hora = hist_Hora[-4:] + ["+5 mins", "+10 mins", "+15 mins", "+20 mins"]
+        show_PM25 = hist_PM25[-4:] + get_predictions(hist_PM25[-4:], 4)
+        show_PM10 = hist_PM10[-4:] + get_predictions(hist_PM10[-4:], 4)
+        show_CO2  = hist_CO2[-4:]  + get_predictions(hist_CO2[-4:],  4)
+        CO2_msg   = get_CO2_msg(show_CO2[-1])
+        PM10_msg  = get_PM10_msg(show_PM10[-1])
+        PM25_msg  = get_PM25_msg(show_PM25[-1])
 
         print("ML prediction done")
 
@@ -220,10 +199,10 @@ def get_ml_predictions():
     else:
         #### NO ENOUGH DATA -> ERROR MSG
         print("NO ENOUGH DATA FOR DOING ML PREDICTIONS")
-        hora_list = list(hora_hist)
-        PM25_list = list(pm25_hist)
-        PM10_list = list(pm10_hist)
-        CO2_list  = list(CO2_hist)
+        show_Hora = hist_Hora
+        show_PM25 = hist_PM25
+        show_PM10 = hist_PM10
+        show_CO2  = hist_CO2
         PM25_msg  = "No ha transcurrido el suficienciente tiempo (<15 mins) para predecir las partículas inferiores a 2,5 micra."
         PM10_msg  = "No ha transcurrido el suficienciente tiempo (<15 mins) para predecir las partículas inferiores a 10 micras."
         CO2_msg   = "No ha transcurrido el suficienciente tiempo (<15 mins) para predecir el CO2."
@@ -233,18 +212,13 @@ def get_ml_predictions():
 
 def fill_data_from_HOPU_and_do_ML():
 
-    global date, time
-
-    get_datetime()
-    print("pipeline at " + date + " " + time)
+    get_datetime() # Aqui se guardan historicos horas
 
     ####### fill data from HOPU and save it into tmp/data.csv as a new row
     API_get_token()
     API_get_device_status(access_token)
-    API_get_calidad_aire(access_token)    
-    API_get_presencia(access_token)
-    print_data()
-    save_data()
+    API_get_calidad_aire(access_token)    # Aqui se guardan historicos calidad aire
+    API_get_presencia(access_token)       # Aqui se guardan historicos personas
 
     ####### Get last 4 rows from tmp/data.csv and do ML predictions
     get_ml_predictions()
@@ -253,14 +227,13 @@ def fill_data_from_HOPU_and_do_ML():
 
 @app.route('/')
 def web_endpoint():
-    global hora_list, CO2_list, PM10_list, PM25_list, CO2_msg, PM10_msg, PM25_msg
     data={
-        "x_labels":   hora_list,
-        "CO2":        CO2_list, #[120, 153, 213, 230, 240, 220, 180, 120],
+        "x_labels":   show_Hora,
+        "CO2":        show_CO2, #[120, 153, 213, 230, 240, 220, 180, 120],
         "CO2_msg":    CO2_msg,
-        "PM10":       PM10_list, #[8, 10, 20, 26, 27, 22, 13, 11],
+        "PM10":       show_PM10, #[8, 10, 20, 26, 27, 22, 13, 11],
         "PM10_msg":   PM10_msg,
-        "PM25":       PM25_list, #[6, 8, 18, 23, 24, 18, 10, 8],
+        "PM25":       show_PM25, #[6, 8, 18, 23, 24, 18, 10, 8],
         "PM25_msg":   PM25_msg
     }
     return render_template('frontend.html', **data)
@@ -268,13 +241,23 @@ def web_endpoint():
 
 @app.route('/data')
 def downloadData ():
-    return send_file("tmp/data.csv", as_attachment=True)
+
+    MAX = 1000
+    data = {
+        "hora": hist_Hora[:MAX],
+        "CO2": hist_CO2 [:MAX],
+        "PM10": hist_PM10[:MAX],
+        "PM25": hist_PM25[:MAX],
+        "Temp": hist_Temperatura[:MAX],
+        "Hume": hist_Humedad[:MAX],
+        "Personas": hist_Personas[:MAX],
+        "PerIn": hist_PersonasIn[:MAX],
+        "PerOut": hist_PersonasOut[:MAX]
+    }
+    return data
 
 
 if __name__ == '__main__':
-
-    init_empty_data()
-    init_ml_model()
     
     scheduler = BackgroundScheduler(timezone='Europe/Madrid') # Default timezone is "utc"
     #scheduler.add_job(fill_data_from_HOPU_and_do_ML, 'interval', seconds=5)
@@ -282,9 +265,5 @@ if __name__ == '__main__':
     scheduler.add_job(fill_data_from_HOPU_and_do_ML, 'cron', day_of_week='mon-fri', hour='7-20', minute='*/5')
     scheduler.start()
 
-    port = os.getenv('PORT') # Port is given by Heroku as environmental variable
-    print("Port:", port)
-
-    #app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
     app.run(host="0.0.0.0")
 
